eda09585927ad55b3166be2e6bd2de2d3058e467
Zeger Hoogeboom <zeger.hoogeboom@gmail.com>
2018-04-11 13:05:06 +0200

    Updated annotation query time filter replacements.

--- public/app/plugins/datasource/influxdb/datasource.ts
+++ public/app/plugins/datasource/influxdb/datasource.ts
@@ -135,9 +135,10 @@ export default class InfluxDatasource {
     }
 
     var timeFilter = this.getTimeFilter({ rangeRaw: options.rangeRaw });
-    var query = options.annotation.query.replace('$timeFilter', timeFilter);
-    query = query.replace('$timeFrom', this.getInfluxTime(options.rangeRaw.from, false));
-    query = query.replace('$timeTo', this.getInfluxTime(options.rangeRaw.to, true));
+    var query = options.annotation.query
+      .replace('$timeFilter', timeFilter)
+      .replace(/\$timeFrom/g, this.getInfluxTime(options.rangeRaw.from, false))
+      .replace(/\$timeTo/g, this.getInfluxTime(options.rangeRaw.to, true));
     query = this.templateSrv.replace(query, null, 'regex');
 
     return this._seriesQuery(query).then(data => {
